dfaec8efd2946acc872c32595b4cc9d836d1d33c
java <java@bigdata>
2018-01-22 22:02:39 +0900

    rename projectname

--- src/test/java/com/example/boardsProjectApplicationTests.java
+++ src/test/java/com/example/boardsProjectApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Springboot06ApplicationTests {
+public class boardsProjectApplicationTests {
 
 	@Test
 	public void contextLoads() {
